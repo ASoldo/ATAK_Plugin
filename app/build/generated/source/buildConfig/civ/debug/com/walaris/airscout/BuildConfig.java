7a15e9dea09409a2d99d0174948d489e2d05ec3a
ASoldo <soldo.andrej@gmail.com>
2025-10-26 20:39:53 +0100

    Update

--- app/build/generated/source/buildConfig/civ/debug/com/walaris/airscout/BuildConfig.java
+++ app/build/generated/source/buildConfig/civ/debug/com/walaris/airscout/BuildConfig.java
@@ -8,8 +8,8 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.walaris.airscout";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "civ";
-  public static final int VERSION_CODE = 1761505261;
-  public static final String VERSION_NAME = "0.1 (c6fb955b) - [5.5.0]";
+  public static final int VERSION_CODE = 1761505637;
+  public static final String VERSION_NAME = "0.1 (73aaf23f) - [5.5.0]";
   // Field from the variant API
   public static final String ATAK_PACKAGE_NAME = "com.atakmap.app.civ";
 }
